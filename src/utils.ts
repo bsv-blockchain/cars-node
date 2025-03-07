8330b13de17cc59a678d4c1fb7b9a72a14f05b0f
Ty J Everett <ty@tyweb.us>
2025-03-06 20:45:16 -0800

    New Overlay Express

--- src/utils.ts
+++ src/utils.ts
@@ -145,7 +145,7 @@ export function generatePackageJson(backendDependencies: Record<string, string>)
     "license": "ISC",
     "dependencies": {
       ...backendDependencies,
-      "@bsv/overlay-express": "^0.3.4",
+      "@bsv/overlay-express": "^0.3.5",
       "mysql2": "^3.11.5",
       "tsx": "^4.19.2",
       "chalk": "^5.3.0"
